da593dc6f840eabc6fc6190b78828fdd62884559
pyhy0101 <545095081@qq.com>
2025-09-28 13:10:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-while True:
     try:
         # 获取用户输入并转换为整数
-        year = int(input("请输入一个年份（输入0退出）："))
-        
-        # 检查是否退出程序
-        if year == 0:
-            print("程序已退出。")
-            break
+        year = int(input("请输入一个年份："))
         
         # 判断是否为闰年
         if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
